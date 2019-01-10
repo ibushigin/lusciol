b14066ddfd9be1b6771bace1682e8103e346d5b8
Vlarue <45257447+Vlarue@users.noreply.github.com>
2019-01-10 21:11:57 +0100

    Pointeur en cours de dev

--- public/js/map.js
+++ public/js/map.js
@@ -4,8 +4,11 @@ $(function(){
         osm = L.tileLayer(osmUrl, {maxZoom: 18, attribution: osmAttrib});
     let map = L.map('map').setView([44.845423, -0.570373], 11).addLayer(osm);
 
-    
+    // var mark1 = '{{loc}}';
+    var lat = '{{lat}}';
+    var long = '{{long}}';
+    // var mark1 = [44.841684, -0.583934]
 
-    var marker = L.marker([44.841684, -0.583934]).addTo(map);
+    var marker = L.marker([lat, long]).addTo(map);
 });
 
