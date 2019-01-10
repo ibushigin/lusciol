5cb7995ac9edd21532e0ca4b486d738946bf3169
ibushigin <rq6v6k3>
2019-01-10 19:51:13 +0100

    marker map

--- public/js/map.js
+++ public/js/map.js
@@ -3,6 +3,9 @@ $(function(){
         osmAttrib = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         osm = L.tileLayer(osmUrl, {maxZoom: 18, attribution: osmAttrib});
     let map = L.map('map').setView([44.845423, -0.570373], 11).addLayer(osm);
+
+    
+
+    var marker = L.marker([44.841684, -0.583934]).addTo(map);
 });
 
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
