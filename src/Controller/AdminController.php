5d1feef31905e62184158d63518316a86c950640
Larue Vincent <45257447+Vlarue@users.noreply.github.com>
2019-01-14 12:13:57 +0100

    mofid admincontroller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Address;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,6 +15,18 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/manageAddress", name="manageAddress")
+     */
+    public function manageAddress(){
+        $repository = $this->getDoctrine()->getRepository(Address::class);
+        $all_address = $repository->findAll();
+
+        return $this->render('admin/manageAddress.html.twig', [
+            'all_address' => $all_address,
         ]);
     }
 }
