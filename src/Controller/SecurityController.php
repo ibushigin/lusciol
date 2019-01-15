2fe3ef37cec5edc2c0e0098fe011a0857fbd2922
Inebhis <inebhis@gmail.com>
2019-01-15 12:14:24 +0100

    déplacement de register dans le security controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,11 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Security\LoginFormAuthenticator;
+use App\Service\FileUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -17,15 +21,81 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request,
+                          UserPasswordEncoderInterface $passwordEncoder,
+                          GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator,
+                          FileUploader $fileuploader): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        // ------------------------------------------------------- //
+
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername,
+            'error' => $error,
+            ]);
     }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register( Request $request,
+                              UserPasswordEncoderInterface $passwordEncoder,
+                              GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator,
+                              FileUploader $fileuploader): Response{
+
+        $form = $this->createForm(RegistrationFormType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            /** @var User */
+
+            $user = $form->getData();
+            dump($user);
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            //suppression du mdp en clair
+            $user->eraseCredentials();
+            //attribution du role par faut à l'utilisateur qui s'inscrit
+            $user->setRoles(['ROLE_USER']);
+
+            //traitement du formulaire d'upload d'image
+            //on m'a envoyé une image
+            $filename = $fileuploader->upload($user->getAvatar(),
+                $this->getParameter('user_avatar_directory'));
+            //j'injecte le nom du fichier dans la propriété image
+            $user->setAvatar($filename);
+
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+
+            // do anything else you need here, like send an email
+
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $authenticator,
+                'main' // firewall name in security.yaml
+            );
+
+        }
+
+        return $this->render('security/register.html.twig',
+            ['registrationForm' => $form->createView()]);
+
+    }
+
 
     /**
      * @Route("/user/infos", name="userInfo")
