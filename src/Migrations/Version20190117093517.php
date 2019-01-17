e6cb3f34f8401e4e0f34495f7fcc15fb92c6f918
Larue Vincent <45257447+Vlarue@users.noreply.github.com>
2019-01-17 10:35:43 +0100

    migrations

--- src/Migrations/Version20190117093517.php
+++ src/Migrations/Version20190117093517.php
@@ -8,14 +8,16 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190114161917 extends AbstractMigration
+final class Version20190117093517 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C8486F9AC');
+        $this->addSql('DROP INDEX IDX_9474526C8486F9AC ON comment');
+        $this->addSql('ALTER TABLE comment DROP adress_id');
     }
 
     public function down(Schema $schema) : void
@@ -23,6 +25,8 @@ final class Version20190114161917 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
+        $this->addSql('ALTER TABLE comment ADD adress_id INT NOT NULL');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C8486F9AC FOREIGN KEY (adress_id) REFERENCES address (id)');
+        $this->addSql('CREATE INDEX IDX_9474526C8486F9AC ON comment (adress_id)');
     }
 }
